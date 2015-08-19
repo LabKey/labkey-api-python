d24e217612181b8ac1d1b3abb48dac37bd08f00a
labkey-nicka <nicka@labkey.com>
2015-08-19 14:46:59 -0700

    Update setup.py. Switch from distutils to setuptools.

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 #
 # Copyright (c) 2011-2015 LabKey Corporation
 #
@@ -15,30 +13,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+"""
+Setup script for Python client API for LabKey Server.
 
-from distutils.core import setup
-setup(name='LabKey',
-      version='0.24',
-      description='Python client API for LabKey Server',
-      long_description = open('README.txt').read() + open('NEWS.txt').read(),
-      license="Apache License 2.0",
-      author='Elizabeth Nelson',
-      author_email='eknelson@labkey.com',
-      maintainer='Brian Connolly',
-      maintainer_email='brian@labkey.com',
-      url='https://www.labkey.org/wiki/home/Documentation/page.view?name=python',
-      download_url='http://www.labkey.com/download-labkey-server',
-      packages=['labkey'],
-      classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 2',
-        'Topic :: Scientific/Engineering'
-        ]
-     )
+Also installs included versions of third party libraries, if those libraries
+are not already installed.
+"""
+from __future__ import print_function
+
+import sys
+
+if sys.version_info < (2, 6):
+  print('labkey-api-python requires python version >= 2.6.',
+        file=sys.stderr)
+  sys.exit(1)
+
+from setuptools import setup
+
+packages = [
+  'labkeyapi'
+]
+
+install_requires = []
+
+long_desc = "Python client API for LabKey Server. Supports for query, wiki, and messageboard APIs."
+
+setup(
+  name='labkey',
+  version='0.24',
+  description='Python client API for LabKey Server',
+  long_description=long_desc
+  license="Apache License 2.0",
+  author='Elizabeth Nelson',
+  author_email='eknelson@labkey.com',
+  maintainer='Brian Connolly',
+  maintainer_email='brian@labkey.com',
+  url='https://www.labkey.org/wiki/home/Documentation/page.view?name=python',
+  install_requires=install_requires
+  packages=packages,
+  package_data={},
+  keywords="labkey api client",
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: MacOS',
+    'Operating System :: Microsoft',
+    'Operating System :: POSIX',
+    'Programming Language :: Python :: 2',
+    'Topic :: Scientific/Engineering'
+  ]
+)
