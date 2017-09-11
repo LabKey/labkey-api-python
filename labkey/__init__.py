34a18e6dd7c82a1714606b8c6baf293f66a5e0d6
Nick Kerr <nicka@labkey.com>
2017-09-11 13:02:34 -0700

    Release 0.6.0

--- labkey/__init__.py
+++ labkey/__init__.py
@@ -16,6 +16,6 @@
 from labkey import query, experiment, security, utils
 
 __title__ = 'labkey'
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 __author__ = 'LabKey'
 __license__ = 'Apache License 2.0'
