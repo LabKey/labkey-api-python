cb54507023d1c79271af17cbd0970924f68038ab
Alan Vezina <alanv@labkey.com>
2020-08-27 13:01:52 -0500

    Clean up test_integration.py

--- test/test_integration.py
+++ test/test_integration.py
@@ -2,11 +2,10 @@ import pytest
 
 from labkey.exceptions import ServerContextError
 from labkey.utils import create_server_context
-from labkey.query import select_rows
-from labkey import query
-from labkey import domain
+from labkey.query import delete_rows, insert_rows, select_rows, update_rows
 from labkey import domain, container
 
+pytestmark = pytest.mark.integration  # Mark all tests in this module as integration tests
 PROJECT_NAME = 'PythonIntegrationTests'
 STUDY_NAME = 'TestStudy'
 SCHEMA_NAME = 'study'
@@ -75,7 +74,7 @@ def study(server_context):
     created_study = server_context.make_request(url, payload, non_json_response=True)
     yield created_study
     url = server_context.build_url('study', 'deleteStudy.view')
-    server_context.make_request(url, None, non_json_response=True)
+    server_context.make_request(url, {'confirm': 'true'}, non_json_response=True)
 
 
 @pytest.fixture(scope="session")
@@ -90,8 +89,8 @@ def dataset(server_context, study):
 
 
 @pytest.fixture(scope="function")
-def test_insert_qcstates(server_context, study):
-    insert_result = query.insert_rows(server_context, 'core', 'qcstate', TEST_QCSTATES)
+def qcstates(server_context, study):
+    insert_result = insert_rows(server_context, 'core', 'qcstate', TEST_QCSTATES)
     yield insert_result
     # clean up
     cleanup_qcStates= [{
@@ -99,10 +98,9 @@ def test_insert_qcstates(server_context, study):
     }, {
         'rowId': insert_result['rows'][1]['rowid']
     }]
-    query.delete_rows(server_context, 'core', 'qcstate', cleanup_qcStates)
+    delete_rows(server_context, 'core', 'qcstate', cleanup_qcStates)
 
 
-@pytest.mark.integration
 def test_select_rows(server_context):
     resp = select_rows(server_context, 'core', 'Users')
     assert resp['schemaName'] == 'core'
@@ -111,12 +109,10 @@ def test_select_rows(server_context):
     assert len(resp['rows']) > 0
 
 
-@pytest.mark.integration
 def test_create_dataset(dataset):
     assert (dataset.name == QUERY_NAME)
 
 
-@pytest.mark.integration
 def test_create_duplicate_dataset(server_context, dataset):
     # Dataset fixture is not used directly here, but it is an argument so it gets created and cleaned up when this test
     # runs
@@ -127,54 +123,48 @@ def test_create_duplicate_dataset(server_context, dataset):
     assert e.value.message == f'\'500: A Dataset or Query already exists with the name "{QUERY_NAME}".\''
 
 
-@pytest.mark.integration
-def test_create_qcstate_definition(server_context, test_insert_qcstates):
-    assert test_insert_qcstates['rowsAffected'] == 2
-    assert test_insert_qcstates['rows'][0]['label'] == 'needs verification'
-    assert test_insert_qcstates['rows'][1]['label'] == 'approved'
+def test_create_qcstate_definition(server_context, qcstates):
+    assert qcstates['rowsAffected'] == 2
+    assert qcstates['rows'][0]['label'] == 'needs verification'
+    assert qcstates['rows'][1]['label'] == 'approved'
 
 
-@pytest.mark.integration
-def test_update_qcstate_definition(server_context, test_insert_qcstates, study):
+def test_update_qcstate_definition(server_context, qcstates, study):
     new_description = 'for sure that is not right'
-    edit_rowid = test_insert_qcstates['rows'][0]['rowid']
-    assert test_insert_qcstates['rows'][0]['description'] != new_description
+    edit_rowid = qcstates['rows'][0]['rowid']
+    assert qcstates['rows'][0]['description'] != new_description
     to_edit_row = [{'rowid': edit_rowid,
                     'description': new_description}]
-    update_response = query.update_rows(server_context, 'core', 'qcstate', to_edit_row);
+    update_response = update_rows(server_context, 'core', 'qcstate', to_edit_row)
     assert update_response['rowsAffected'] == 1
     assert update_response['rows'][0]['description'] == new_description
 
 
-@pytest.mark.integration
-def test_insert_duplicate_labeled_qcstate_produces_error(server_context, test_insert_qcstates, study):
-
+def test_insert_duplicate_labeled_qcstate_produces_error(server_context, qcstates, study):
     with pytest.raises(ServerContextError) as e:
         dupe_qcstate = [{
             'label': 'needs verification',
             'publicData': 'false'
         }]
-        query.insert_rows(server_context, 'core', 'qcstate', dupe_qcstate)
+        insert_rows(server_context, 'core', 'qcstate', dupe_qcstate)
     assert "500: ERROR: duplicate key value violates unique constraint" in e.value.message
 
 
-@pytest.mark.integration
-def test_cannot_delete_qcstate_in_use(server_context, test_insert_qcstates, study, dataset):
-    qcstate_rowid = test_insert_qcstates['rows'][0]['rowid']
+def test_cannot_delete_qcstate_in_use(server_context, qcstates, study, dataset):
+    qcstate_rowid = qcstates['rows'][0]['rowid']
     new_row = [{'ParticipantId': '2',
                 'vitd': 4,
                 'SequenceNum': '345',
                 'QCState': qcstate_rowid}]
-    existing_rows = query.select_rows(server_context, SCHEMA_NAME, QUERY_NAME)['rows']
-    insert_result = query.insert_rows(server_context, SCHEMA_NAME, QUERY_NAME, new_row)
+    insert_result = insert_rows(server_context, SCHEMA_NAME, QUERY_NAME, new_row)
     inserted_lsid = insert_result['rows'][0]['lsid']
     assert insert_result['rowsAffected'] == 1
     assert insert_result['rows'][0]['QCState'] == qcstate_rowid
 
     with pytest.raises(ServerContextError) as e:
         qcstate_to_delete = [{'rowid': qcstate_rowid}]
-        query.delete_rows(server_context, 'core', 'qcstate', qcstate_to_delete)
+        delete_rows(server_context, 'core', 'qcstate', qcstate_to_delete)
     assert  e.value.message == '"400: QC state \'needs verification\' cannot be deleted as it is currently in use."'
     # now clean up/stop using it
     dataset_row_to_remove = [{'lsid': inserted_lsid}]
-    query.delete_rows(server_context, SCHEMA_NAME, QUERY_NAME, dataset_row_to_remove)
+    delete_rows(server_context, SCHEMA_NAME, QUERY_NAME, dataset_row_to_remove)
