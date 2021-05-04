32e1bfcde9cada4a99df2326d4c64662f62bd514
ian <ians@labkey.com>
2021-05-04 10:41:15 -0700

    Bump version to 2.1.0 and update change log.

--- labkey/__init__.py
+++ labkey/__init__.py
@@ -16,6 +16,6 @@
 from labkey import domain, query, experiment, security, utils
 
 __title__ = "labkey"
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 __author__ = "LabKey"
 __license__ = "Apache License 2.0"
