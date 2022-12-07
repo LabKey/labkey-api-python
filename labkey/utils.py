ca6eb7ddecb4c6595a4eb325f51885dfc7680e16
jryurkanin <104916543+jryurkanin@users.noreply.github.com>
2022-12-06 16:53:40 -0800

    adding helper function to utils.py
    The helper function parses the run properties and results data so that the user doesn't need to include any parsing script in their transform script. The helper function only accepts a user-defined function that accepts an array of results data as its input and returns an array of results data matching the assay design it is used with. Also, the helper function requires that users explicitly define the run properties in their transform script, otherwise the labkey transform script parser does not recognize ${runInfo}.

--- labkey/utils.py
+++ labkey/utils.py
@@ -31,3 +31,44 @@ class DateTimeEncoder(json.JSONEncoder):
 def json_dumps(*args, **kwargs):
     kwargs.setdefault("cls", DateTimeEncoder)
     return json.dumps(*args, **kwargs)
+
+
+def transform_helper(userTransformFunc, filePathRunProperties):
+    #filePathRunProperties must be explicitly defined as ${runInfo} within the user's transform script
+    #parse run properties to for results data in and out filepaths
+    filePathIn = ''
+    filePathOut = ''
+    fileRunProperties = open(filePathRunProperties) 
+    for l in fileRunProperties:
+        row = l.strip().split('\t')
+        if row[0] == 'runDataFile':
+            filePathIn = row[1]
+            filePathOut = row[3]
+    fileRunProperties.close()
+    
+    #parse results data into array, confirming supported file type is used
+    fileIn = open(filePathIn)
+    dataGrid = []
+    
+    for l in fileIn:
+        if '\t' in l:
+            row = l.replace('\n', '').split('\t')
+        elif ',' in l:
+            row = l.replace('\n', '').split(',')
+        else:
+            raise ValueError('Unsupported file type or delimiter used. Header used: \n' + str(l))
+        dataGrid.append(row)    
+    fileIn.close()
+    
+    #run user transform on parsed results data array
+    transformedGrid = userTransformFunc(dataGrid)
+    
+    #write transformed results data array to LabKey assay results data grid
+    #transformed array must be a python list object, not a numpy array or pandas dataframe
+    fileOut = open(filePathOut, mode='w')
+    for row in transformedGrid:
+        row = [str(el).strip() for el in row]
+        row = '\t'.join(row)        
+        fileOut.write(row + '\n')
+    
+    fileOut.close()
