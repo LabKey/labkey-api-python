ee37c3d0e781a7328fad4289c695c1a919196966
jryurkanin <104916543+jryurkanin@users.noreply.github.com>
2024-04-14 21:48:28 -0700

    Update experiment_example.py
    replace project_name with container_path

--- samples/experiment_example.py
+++ samples/experiment_example.py
@@ -17,9 +17,9 @@ from labkey.api_wrapper import APIWrapper
 from labkey.experiment import Batch, Run
 
 labkey_server = "localhost:8080"
-project_name = "ModulesAssayTest"  # Project folder name
+container_path = 'Tutorials/HIV Study' # Full project/folder container path
 context_path = "labkey"
-api = APIWrapper(labkey_server, project_name, context_path, use_ssl=False)
+api = APIWrapper(labkey_server, container_path, context_path, use_ssl=False)
 
 assay_id = 3315  # provide one from your server
 
