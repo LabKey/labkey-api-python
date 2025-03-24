914e1397d268d428abf4d5df0dd78cdb8143b3be
Cory Nathe <cnathe@labkey.com>
2025-03-24 09:51:16 -0500

    Add "valueExpression" to PropertyDescriptor class (needed for creating/adding domain calculated fields) (#79)
    - add "valueExpression" to PropertyDescriptor class
    - update Domain class to append calculatedFields to the domain's fields
    - add integration test for adding calculated fields to a domain

--- labkey/domain.py
+++ labkey/domain.py
@@ -109,6 +109,7 @@ class PropertyDescriptor:
         )
         self.type_editable = kwargs.pop("type_editable", kwargs.pop("typeEditable", None))
         self.url = kwargs.pop("url", None)
+        self.value_expression = kwargs.pop("value_expression", kwargs.pop("valueExpression", None))
 
     def to_json(self, strip_none=True):
         # TODO: Likely only want to include those that are not None
@@ -155,6 +156,7 @@ class PropertyDescriptor:
             "shownInUpdateView": self.shown_in_update_view,
             "typeEditable": self.type_editable,
             "url": self.url,
+            "valueExpression": self.value_expression,
         }
 
         json_formats = []
@@ -232,9 +234,11 @@ class Domain:
             "template_description", kwargs.pop("templateDescription", None)
         )
 
-        fields = kwargs.pop("fields", [])
         fields_instances = []
-
+        fields = kwargs.pop("fields", [])
+        for field in fields:
+            fields_instances.append(PropertyDescriptor(**field))
+        fields = kwargs.pop("calculated_fields", kwargs.pop("calculatedFields", []))
         for field in fields:
             fields_instances.append(PropertyDescriptor(**field))
 
