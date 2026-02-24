166766a4c70a8e88ebda1530ad152203ba98baa4
alanv <alanv@labkey.com>
2026-02-24 13:40:38 -0600

    Update build to use pyproject.toml and hatch
    Remove setup.py

--- labkey/__init__.py
+++ labkey/__init__.py
@@ -13,7 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-__title__ = "labkey"
-__version__ = "4.0.2"
-__author__ = "LabKey"
-__license__ = "Apache License 2.0"
